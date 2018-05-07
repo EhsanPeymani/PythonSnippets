8191eca1cfeeb5f6fd9e0554b34f081ec77366e6
ehsan.peymani.f@gmail.com <ehsanTO2>
2018-05-07 12:05:34 +0200

    sort dicts

---
+++ sort_dictionary.py
@@ -0,0 +1,18 @@
+# Dan Bader
+
+my_dict = dict(a=5, c=3, b=10, e=2, d=9)
+
+# sorted will return a list of sorted keys
+sorted_dict_1 = sorted(my_dict)
+print('sorted(my_dict):', sorted_dict_1)
+
+# sort dictionary by value -- method 1
+sorted_dict_2 = sorted(my_dict.items(), key=lambda item: item[1])
+print('sorted(my_dict.items(), key=lambda item: item[1]):', sorted_dict_2)
+print('sorted(my_dict.items(), key=lambda item: item[1]):', dict(sorted_dict_2))
+
+# sort dictionary by value -- method 2
+import operator
+sorted_dict_3 = sorted(my_dict.items(), key=operator.itemgetter(1))
+print('sorted(my_dict.items(), key=operator.itemgetter(1)):', sorted_dict_3)
+print('sorted(my_dict.items(), key=operator.itemgetter(1)):', dict(sorted_dict_3))
