5a044fdd15f4dc7f9b7e018e9f732dd1a8470130
ehsan.peymani.f@gmail.com <ehsanTO2>
2018-05-23 12:40:52 +0200

    counting generators

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -9,10 +9,10 @@ print('sorted(my_dict):', sorted_dict_1)
 # sort dictionary by value -- method 1
 sorted_dict_2 = sorted(my_dict.items(), key=lambda item: item[1])
 print('sorted(my_dict.items(), key=lambda item: item[1]):', sorted_dict_2)
-print('sorted(my_dict.items(), key=lambda item: item[1]):', dict(sorted_dict_2))
+print('dict(sorted(my_dict.items(), key=lambda item: item[1])):', dict(sorted_dict_2))
 
 # sort dictionary by value -- method 2
 import operator
 sorted_dict_3 = sorted(my_dict.items(), key=operator.itemgetter(1))
 print('sorted(my_dict.items(), key=operator.itemgetter(1)):', sorted_dict_3)
-print('sorted(my_dict.items(), key=operator.itemgetter(1)):', dict(sorted_dict_3))
+print('dict(sorted(my_dict.items(), key=operator.itemgetter(1))):', dict(sorted_dict_3))
